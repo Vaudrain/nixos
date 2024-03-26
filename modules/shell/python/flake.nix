8a5751e50f2b2abf43c896d6bb046db4d6b732e5
Nic <nxb484@student.bham.ac.uk>
2024-03-26 16:34:47 +0000

    Added example python dev flake

---
+++ modules/shell/python/flake.nix
@@ -0,0 +1,31 @@
+## Add to any Python project directory and run nix develop (or use direnv)
+
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            poetry
+            python3
+            jupyter
+          ];
+          shellHook = ''
+          echo "Testing - activating venv"
+          VENV=.venv
+          if test ! -d $VENV; then
+            virtualenv $VENV
+          fi
+          source ./$VENV/bin/activate
+          '';
+        };
+      });
+    };
+}
