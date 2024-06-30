16ffb19e52166a35f4bd67e4cfb0e0c1a8559feb
Nic <nxb484@student.bham.ac.uk>
2024-06-30 16:10:44 +0100

    vesktop derivation

--- modules/programs/communication.nix
+++ modules/programs/communication.nix
@@ -1,7 +1,10 @@
-{ pkgs, user, ... }:
+{ pkgs, user, fetchFromGitHub, lib,  ... }:
 
+let
+  vesktop = pkgs.callPackage ./derivations/vesktop.nix { };
+in
 {
-  home-manager.users.${user}.home.packages = with pkgs; [
+  home-manager.users.${user}.home.packages = with pkgs; [      
       vesktop
       zoom-us
       teams-for-linux
