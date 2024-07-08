370c6b45f870088b9726179509e799fd069c45bd
Nic <nxb484@student.bham.ac.uk>
2024-07-08 15:53:33 +0100

    Plasma browser integration

--- modules/programs/firefox/floorp.nix
+++ modules/programs/firefox/floorp.nix
@@ -2,7 +2,7 @@
 
 let
   floorp-package = pkgs.wrapFirefox pkgs.floorp-unwrapped {
-    nativeMessagingHosts = [ (pkgs.callPackage ./floorp-profile-switcher-connector.nix { }) ]; # This doesn't appear to work, but it's fine for now
+    nativeMessagingHosts = [ pkgs.plasma-browser-integration (pkgs.callPackage ./floorp-profile-switcher-connector.nix { }) ]; # Profile switcher doesn't appear to work, but it's fine for now
     extraPolicies = { ExtensionSettings = { }; };
   };
 in {
