8dc02b836a672ec0da000a42a6a35e0051f69010
Nic <nxb484@student.bham.ac.uk>
2024-09-03 17:29:03 +0100

    Updated floorp on toolbar

--- modules/desktops/plasma.nix
+++ modules/desktops/plasma.nix
@@ -45,7 +45,7 @@
                 "applications:org.kde.dolphin.desktop"
                 "applications:org.kde.konsole.desktop"
                 "applications:obsidian.desktop"
-                "applications:one.ablaze.floorp.desktop"
+                "applications:floorp.desktop"
                 "applications:vesktop.desktop"
               ];
             };
