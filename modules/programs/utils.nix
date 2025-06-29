56886cd3ed0f590fd15bf627961a889a11488225
Nic <nxb484@student.bham.ac.uk>
2025-06-29 13:30:22 +0100

    Added remmina - pinned to version before multi-monitor bug

--- modules/programs/utils.nix
+++ modules/programs/utils.nix
@@ -1,7 +1,18 @@
 { pkgs, user, ... }:
 
+let
+  pinnedRemmina = import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "c2a03962b8e24e669fb37b7df10e7c79531ff1a4";
+    sha256 = "1klhr7mrfhrzcqfzngk268jspikbivkxg96x2wncjv1ik3zb8i75";
+  }) {
+    inherit (pkgs) system;
+  };
+in
 {
   home-manager.users.${user}.home.packages = with pkgs; [
+pinnedRemmina.remmina
       docker
       polychromatic
       xwayland
