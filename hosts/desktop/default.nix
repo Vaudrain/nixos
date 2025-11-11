3295f2ecf4b9baac68284054a9b3a6a31307a47e
vaud <vaurigg@gmail.com>
2025-11-11 20:14:59 +0000

    Updated GPU driver, added vdpau driver for screensharing efficiency

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -51,7 +51,6 @@
 
   nixpkgs.config.allowUnfree = true; # For Nvidia drivers
   nixpkgs.config.allowUnsupportedSystem = true;
-  #nixpkgs.config.cudaSupport = true; # For Cuda
 
 
   services.pulseaudio.enable = false;
@@ -76,15 +75,15 @@
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = with pkgs; [ vaapiVdpau ];
+      extraPackages = with pkgs; [ libva-vdpau-driver ];
     };
   };
 
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "580.95.05"; # To update - change version number, try to rebuild, replace sha hashes # See https://www.nvidia.com/en-gb/geforce/drivers/ for updates
-    sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-    openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-    settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
-    persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
+    version = "580.105.08"; # To update - change version number, try to rebuild, replace sha hashes # See https://www.nvidia.com/en-gb/geforce/drivers/ for updates
+    sha256_64bit = "sha256-2cboGIZy8+t03QTPpp3VhHn6HQFiyMKMjRdiV2MpNHU=";
+    openSha256 = "sha256-FGmMt3ShQrw4q6wsk8DSvm96ie5yELoDFYinSlGZcwQ=";
+    settingsSha256 = "sha256-YvzWO1U3am4Nt5cQ+b5IJ23yeWx5ud1HCu1U0KoojLY=";
+    persistencedSha256 = "sha256-qh8pKGxUjEimCgwH7q91IV7wdPyV5v5dc5/K/IcbruI=";
   };
 }
